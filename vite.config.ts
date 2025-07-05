d19b3597646e11fa05b2f3c4a8ba8b781eddbd25
vile319 <vile319@gmail.com>
2025-07-05 16:54:44 -0400

    Fix base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/TwelveReader-App/',
+    base: '/',
     plugins: [react()],
     build: {
       target: 'esnext',
